918960e7fba89086d60385ced999e6ceb3bd94eb
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-07 18:19:33 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,11 @@ const nameInputEl = document.querySelector("#name-input");
 const textOutputEl = document.querySelector("#name-output");
 
 function checkTextArea(event) {
-    if (nameInputEl.textContent = "") {
-        textOutputEl.textContent = "Anonimous";
-    } else {
+    if ( event.currentTarget.value !== "") {
         textOutputEl.textContent = event.currentTarget.value;
+    } else {
+        textOutputEl.textContent = "Anonymous";
     }
 }
 nameInputEl.addEventListener("input", checkTextArea);
+
