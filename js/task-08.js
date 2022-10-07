9d9e33d23d1de9775c5b4d120bcb359a12c4bcae
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2022-10-07 19:33:51 +0300

    08,09 tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+
+  const formDataEl = {
+    email: formEl.email.value,
+    password: formEl.password.value,
+  };
+
+  if (formDataEl.email === "" || formDataEl.password === "") {
+    window.alert(
+      "Please fill in all fields and try to login again"
+    );
+  } else {
+    form.reset();
+    console.log(formDataEl);
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeColorBtn = document.querySelector(".change-color");
+const currentColorPage = document.querySelector(".color");
+const page = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  page.style.backgroundColor = getRandomHexColor();
+  currentColorPage.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
